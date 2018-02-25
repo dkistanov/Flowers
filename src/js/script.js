c2019ef3aad6d4b2a11123ec23e46fa59f9b8e82
Daniil <rizzydizzy@mail.ru>
2018-02-25 23:12:46 +0300

    Finish

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,37 @@ let place = 'Flavortown';
 
 console.log( `Hello ${name}, ready for ${place}?` );
 
+const sliderControls = document.querySelector('.reviews__slider-controls');
+const sliderControl = document.querySelectorAll('.slider__control');
+const sliderList = document.querySelector('.reviews__list');
+
+sliderControls.addEventListener('click', function(e){
+        const _this = e.target;
+        if (!_this.classList.contains('active')) {
+            for (let i = 0; i < sliderControl.length; i++) {
+                sliderControl[i].classList.remove('active');
+            }
+            var scroll = _this.dataset.test;
+            console.log(scroll)
+            sliderList.style.left = "0";
+            sliderList.style.left = `${scroll * -100}%`;
+            _this.classList.add('active');
+
+        };
+
+        //  else {
+        //     sliderList.style.left = `${active.dataset.test * -100}%`;
+        // }
+
+});
+
+$(document).ready(function(){
+    $('.nav__link').click( function(){
+	var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) { 
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); 
+        }
+	    return false;
+    });
+});
+
